4ab3a4dfd6a5aef358289dd0f35be7c2c5ad2ced
Nkululeko Mbhele <47152995+NkululekoMbhele@users.noreply.github.com>
2021-07-18 01:07:42 +0200

    fix the user interface

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from tkinter import *
 from pytube import YouTube 
 
 
+ #to_do 
+
 def download_video():
     link = url_input.get()
     
-    SAVE_PATH = "./" #to_do 
-    #link="https://www.youtube.com/watch?v=nC9dQOnUyao"
     try: 
         youtube = YouTube(link) 
     except: 
@@ -16,36 +16,46 @@ def download_video():
     d_video = youtube.streams.get_highest_resolution()
     try: 
         # downloading the video 
-        d_video.download() 
+        d_video.download(file_path.cget("text")) 
     except: 
         print() 
         result_label.config(text="Some Error!")
     print() 
     result_label.config(text='Task Completed!')
-    
 
+
+SAVE_PATH = ""
+
+def choose_folder():
+    SAVE_PATH = str(filedialog.askdirectory())
+    file_path.config(text=SAVE_PATH)
+    
 window = Tk()
 window.title("Download Youtube Videos")
-window.config(padx=20, pady=20)
-
-
-url_input = Entry(width=20)
-url_input.grid(column=1, row=1)
-
-folder_selected = filedialog.askdirectory()
+window.config(width=300, height=300, padx=100, pady=100)
 
 
 
-url_label = Label(text="Enter the video url")
+url_input = Entry(width=50)
+url_input.grid(column=1, row=0)
+
+
+url_label = Label(text="Enter the video url: ", pady=20)
+url_label.grid(column=0, row=0)
+
+url_label = Label(text="File path: ")
 url_label.grid(column=0, row=1)
 
+download_button = Button(text="Browse", command=choose_folder)
+download_button.grid(column=2, row=1)
 
-result_label = Label(text="")
-result_label.grid(column=2, row=2)
+file_path = Label(text="./", pady=20)
+file_path.grid(column=1, row=1)
+
+result_label = Label(text="", pady=20)
+result_label.grid(column=1, row=3)
 
 
 download_button = Button(text="Download", command=download_video)
 download_button.grid(column=1, row=2)
-
-
 window.mainloop()
